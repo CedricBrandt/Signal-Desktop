221749f2d53db0dd96c1b671ebd4856841223f32
Scott Nonnenberg <scott@nonnenberg.com>
2017-07-06 15:47:19 -0700

    Try for cleaner windows path management solution
    FREEBIE

---
+++ shorten_win_path.bat
@@ -0,0 +1,25 @@
+rem Taken from here: https://stackoverflow.com/questions/4405091/how-do-you-avoid-over-populating-the-path-environment-variable-in-windows
+
+@echo off
+
+SET MyPath=%PATH%
+echo "Old PATH:"
+echo %MyPath%
+echo --------
+
+setlocal EnableDelayedExpansion
+
+SET TempPath="%MyPath:;=";"%"
+SET var=
+FOR %%a IN (%TempPath%) DO (
+    IF exist %%~sa (
+        SET "var=!var!;%%~sa"
+    ) ELSE (
+        echo %%a does not exist
+    )
+)
+
+echo -------
+echo "New PATH:"
+echo %var%
+set PATH=%var%
